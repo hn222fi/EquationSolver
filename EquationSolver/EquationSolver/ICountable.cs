84893ec458abed24310e78c7c1f2728837e68905
Henrik Nilsson <hn222fi@github.com>
2014-10-25 12:16:57 +0200

    Skapat interfacet ICountable som innehåller metoderna Add, Subtract, Multiply and Divide.

---
+++ EquationSolver/EquationSolver/ICountable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationSolver
+{
+    interface ICountable
+    {
+        ICountable Add
+        { }
+        ICountable Subtract
+        { }
+        ICountable Multiply
+        { }
+        ICountable Divide
+        { }
+    }
+}
